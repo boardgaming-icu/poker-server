76d966acab6bc8a3f0dc2ca6eceb4dc87c72bb1d
matthew-st <me@ocld.cc>
2024-06-06 19:27:14 +0100

    add initial template code and folders

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "coverageThreshold": {
+        "global": {
+            "branches": 65,
+            "functions": 65,
+            "lines": 65,
+            "statements": 65
+        }
+    }
+}
